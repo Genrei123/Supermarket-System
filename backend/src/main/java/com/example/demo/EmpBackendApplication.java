0f6662f0b746a43156beb2836fa53863af09942d
Genrey <genreycristobal03@gmail.com>
2024-08-11 10:11:15 +0800

    Added receipt logic

--- backend/src/main/java/com/example/demo/EmpBackendApplication.java
+++ backend/src/main/java/com/example/demo/EmpBackendApplication.java
@@ -1,14 +1,20 @@
 package com.example.demo;
 
+import com.example.demo.Receipt.ReceiptMaker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 @SpringBootApplication
 @CrossOrigin(origins = "http://localhost:4200")
 public class EmpBackendApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		ReceiptMaker receiptMaker = new ReceiptMaker();
+		receiptMaker.generateReceipt("TEST1");
 		SpringApplication.run(EmpBackendApplication.class, args);
 	}
 
